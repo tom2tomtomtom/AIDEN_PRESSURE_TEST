dede0d069ae069b69d9866fcb1624a6fa1affddd
tom2tomtomtotm <154282811+tom2tomtomtom@users.noreply.github.com>
2026-02-02 11:20:36 +1100

    fix: externalize @react-pdf/renderer to prevent bundling conflicts

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  // Externalize @react-pdf/renderer to prevent bundling conflicts
+  // This fixes React version conflicts in production
+  serverExternalPackages: [
+    '@react-pdf/renderer',
+    '@react-pdf/layout',
+    '@react-pdf/pdfkit',
+    '@react-pdf/primitives',
+    '@react-pdf/reconciler',
+    '@react-pdf/font',
+    '@react-pdf/image',
+    '@react-pdf/stylesheet',
+    '@react-pdf/textkit',
+  ],
   images: {
     remotePatterns: [
       {
